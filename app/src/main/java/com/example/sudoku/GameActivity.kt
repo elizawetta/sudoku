02a52d4de6919012e054f27573a2a155b7e5c0a3
BarbedRuff <Barbed.Ruff@yandex.ru>
2023-12-23 17:35:51 +0300

    create visual click buttons

--- app/src/main/java/com/example/sudoku/GameActivity.kt
+++ app/src/main/java/com/example/sudoku/GameActivity.kt
@@ -1,18 +1,47 @@
 package com.example.sudoku
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Chronometer
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        // Chronometer
         var chronometer = findViewById<Chronometer>(R.id.chronometer)
         chronometer.setFormat("%m:%s")
         var startTime = SystemClock.elapsedRealtime()
         chronometer.setBase(startTime)
         chronometer.start()
+        // Buttons
+        var home = findViewById<ImageButton>(R.id.home);
+        var write = findViewById<ImageButton>(R.id.write);
+        var delete = findViewById<ImageButton>(R.id.delete);
+        var restart = findViewById<ImageButton>(R.id.restart);
+        // OnClickListeners
+        home.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        write.setOnClickListener{
+            write.setImageResource(R.drawable.write_active)
+            delete.setImageResource(R.drawable.delete)
+        }
+        delete.setOnClickListener{
+            write.setImageResource(R.drawable.write)
+            delete.setImageResource(R.drawable.delete_active)
+        }
+        restart.setOnClickListener{
+            write.setImageResource(R.drawable.write)
+            delete.setImageResource(R.drawable.delete)
+            Toast.makeText(this, "restart", 3).show()
+        }
     }
 }
